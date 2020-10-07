c9efce2014bfdf450dbf02e136bf489cb1185bca
Alan Lima <alan.lima123@hotmail.com>
2020-10-07 14:15:23 -0300

    Cliente, TipoCliente, telefones e endereços

---
+++ src/main/java/com/alanlima/cursospring/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.alanlima.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alanlima.cursospring.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
