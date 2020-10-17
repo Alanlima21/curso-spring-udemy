e24076c69a8b38716234c4adf672ba031a5e3209
Alan Lima <alan.lima123@hotmail.com>
2020-10-16 21:22:11 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/alanlima/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/alanlima/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.alanlima.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alanlima.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
