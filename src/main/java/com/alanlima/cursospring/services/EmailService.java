5178ad24515bcb8c54226a83d5a8e8ce73cc3c4e
Alan Lima <alan.lima123@hotmail.com>
2020-10-27 21:46:19 -0300

    Email HTML

--- src/main/java/com/alanlima/cursospring/services/EmailService.java
+++ src/main/java/com/alanlima/cursospring/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.alanlima.cursospring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.alanlima.cursospring.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void senderOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
