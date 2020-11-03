d636bac7fd346db6bbd9f2e7bd5ed5c4ae098389
Alan Lima <alan.lima123@hotmail.com>
2020-11-03 20:35:49 -0300

    Implementando autorização

--- src/main/java/com/alanlima/cursospring/config/SecurityConfig.java
+++ src/main/java/com/alanlima/cursospring/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alanlima.cursospring.security.JWTAuthenticationFilter;
+import com.alanlima.cursospring.security.JWTAuthorizationFilter;
 import com.alanlima.cursospring.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDatailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
