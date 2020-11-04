32e60c1505e3207df85fbec4f127fe081e57ee9f
Alan Lima <alan.lima123@hotmail.com>
2020-11-04 20:02:43 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/alanlima/cursospring/config/SecurityConfig.java
+++ src/main/java/com/alanlima/cursospring/config/SecurityConfig.java
@@ -16,9 +16,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.alanlima.cursospring.security.JWTAuthenticationFilter;
 import com.alanlima.cursospring.security.JWTAuthorizationFilter;
@@ -67,6 +69,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDatailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		CharacterEncodingFilter filter = new CharacterEncodingFilter(); 
+		filter.setEncoding("UTF-8"); 
+		filter.setForceEncoding(true); 
+		http.addFilterBefore(filter, CsrfFilter.class);
 	}
 	
 	@Override
